4b3645fcfdff6651ae7da18f86d5942ad95f8459
gb10aac <gb10aac@LCS-LISQFAP-LT2.herts.ac.uk>
2019-03-03 06:40:15 +0000

    Add project files.

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository TheRepository;
+        private readonly IMapper Mapper;
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            TheRepository = repository;
+            Mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CampModel[]>> GetCamps()
+        {
+            try
+            {
+                var results = await TheRepository.GetAllCampsAsync();
+                return Mapper.Map<CampModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failed");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await TheRepository.GetCampAsync(moniker);
+
+                if (result is null)
+                {
+                    return NotFound();
+                }
+
+                return Mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failed");
+            }
+        }
+    }
+}
