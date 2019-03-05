235221cfd454cbfe48d3cc133004671126202b13
Greg Bowie <greg_bowie@yahoo.co.uk>
2019-03-05 20:05:41 +0000

    Using URL Versioning (not recommended) example

--- Startup.cs
+++ Startup.cs
@@ -28,10 +28,11 @@ namespace CoreCodeCamp
             opt.AssumeDefaultVersionWhenUnspecified = true;
             opt.DefaultApiVersion = new ApiVersion(1, 1);
             opt.ReportApiVersions = true;
-            opt.ApiVersionReader = ApiVersionReader.Combine(
-                new HeaderApiVersionReader("X-Version"),
-                new QueryStringApiVersionReader("ver", "version")
-            );
+            opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+            //opt.ApiVersionReader = ApiVersionReader.Combine(
+            //    new HeaderApiVersionReader("X-Version"),
+            //    new QueryStringApiVersionReader("ver", "version")
+            //);
         });
         services.AddMvc()
             .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
